1f678ae9f179c756813385a1669e0db9de4bf3ff
Ada Michocka <amichocka@wp.pl>
2021-05-06 20:21:22 +0200

    init commit

---
+++ src/test/java/pp_labo_3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pp_labo_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
